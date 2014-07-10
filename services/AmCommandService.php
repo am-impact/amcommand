daafcec66a9bff1dadf8d7664bd7e43edd715063
Hubert Prein <h.prein@am-impact.nl>
2014-07-10 13:35:59 +0200

    a&m command 0.2 update

--- services/AmCommandService.php
+++ services/AmCommandService.php
@@ -10,67 +10,48 @@ class AmCommandService extends BaseApplicationComponent
      */
     public function getAvailableSearches()
     {
-        $data = $this->_getSettings();
-        $data = array_merge($data, $this->_getSections());
+        $data = array(
+            Craft::t('Settings')  => $this->_getSettings(),
+            Craft::t('New Entry') => $this->_getSections()
+        );
         return $data;
     }
 
     /**
      * Get useful settings.
+     *
+     * @return array
      */
     private function _getSettings()
     {
+        if (! craft()->userSession->isAdmin()) {
+            return array();
+        }
         $settings = array(
             // Overview
-            array(
-                'type' => Craft::t('Settings'),
-                'name' => Craft::t('Fields'),
-                'url'  => UrlHelper::getUrl('settings/fields')
-            ),
-            array(
-                'type' => Craft::t('Settings'),
-                'name' => Craft::t('Globals'),
-                'url'  => UrlHelper::getUrl('settings/globals')
-            ),
-            array(
-                'type' => Craft::t('Settings'),
-                'name' => Craft::t('Users'),
-                'url'  => UrlHelper::getUrl('settings/users')
-            ),
-            array(
-                'type' => Craft::t('Settings'),
-                'name' => Craft::t('Sections'),
-                'url'  => UrlHelper::getUrl('settings/sections')
-            ),
+            Craft::t('Fields')      => UrlHelper::getUrl('settings/fields'),
+            Craft::t('Globals')     => UrlHelper::getUrl('settings/globals'),
+            Craft::t('Users')       => UrlHelper::getUrl('settings/users'),
+            Craft::t('Sections')    => UrlHelper::getUrl('settings/sections'),
             // New
-            array(
-                'type' => Craft::t('Settings'),
-                'name' => Craft::t('New Field'),
-                'url'  => UrlHelper::getUrl('settings/fields/new')
-            ),
-            array(
-                'type' => Craft::t('Settings'),
-                'name' => Craft::t('New Section'),
-                'url'  => UrlHelper::getUrl('settings/sections/new')
-            )
+            Craft::t('New Field')   => UrlHelper::getUrl('settings/fields/new'),
+            Craft::t('New Section') => UrlHelper::getUrl('settings/sections/new')
         );
         return $settings;
     }
 
     /**
      * Get all available sections.
+     *
+     * @return array
      */
     private function _getSections()
     {
         $sections = array();
         $availableSections = craft()->sections->getAllSections();
         foreach ($availableSections as $section) {
-            if ($section->type != SectionType::Single) {
-                $sections[] = array(
-                    'type' => Craft::t('New Entry'),
-                    'name' => $section->name,
-                    'url'  => UrlHelper::getUrl('entries/' . $section->handle . '/new')
-                );
+            if ($section->type != SectionType::Single && craft()->userSession->checkPermission('editEntries:'.$section->id)) {
+                $sections[$section->name] = UrlHelper::getUrl('entries/' . $section->handle . '/new');
             }
         }
         return $sections;
