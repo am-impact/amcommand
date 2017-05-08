f5bc4d5de1769703211b992576f75e808132b18f
Hubert Prein <h.prein@am-impact.nl>
2017-05-08 12:27:38 +0200

    Fix PHP 5.3 support

--- services/AmCommandService.php
+++ services/AmCommandService.php
@@ -267,8 +267,9 @@ class AmCommandService extends BaseApplicationComponent
      */
     private function _sortCommands($commands)
     {
-        usort($commands, function($a, $b) {
-            return $this->_reverseSorting ? strnatcmp($b['name'], $a['name']) : strnatcmp($a['name'], $b['name']);
+        $reverseSorting = $this->_reverseSorting;
+        usort($commands, function($a, $b) use ($reverseSorting) {
+            return $reverseSorting ? strnatcmp($b['name'], $a['name']) : strnatcmp($a['name'], $b['name']);
         });
         return $commands;
     }
@@ -535,28 +536,28 @@ class AmCommandService extends BaseApplicationComponent
                 'name' => Craft::t('Settings') . ': ' . Craft::t('Fields'),
                 'url'  => UrlHelper::getUrl('settings/fields')
             );
-            $currentCommands[] = [
+            $currentCommands[] = array(
                 'name'    => Craft::t('Settings') . ': ' . Craft::t('Fields') . ' - ' . Craft::t('Edit'),
                 'more'    => true,
                 'call'    => 'editFields',
                 'service' => 'amCommand_settings'
-            ];
-            $currentCommands[] = [
+            );
+            $currentCommands[] = array(
                 'name'    => Craft::t('Settings') . ': ' . Craft::t('Fields') . ' - ' . Craft::t('Duplicate'),
                 'more'    => true,
                 'call'    => 'duplicateFields',
                 'service' => 'amCommand_settings'
-            ];
+            );
             $currentCommands[] = array(
                 'name' => Craft::t('Settings') . ': ' . Craft::t('Sections'),
                 'url'  => UrlHelper::getUrl('settings/sections')
             );
-            $currentCommands[] = [
+            $currentCommands[] = array(
                 'name'    => Craft::t('Settings') . ': ' . Craft::t('Sections') . ' - ' . Craft::t('Edit'),
                 'more'    => true,
                 'call'    => 'editSections',
                 'service' => 'amCommand_settings'
-            ];
+            );
             $currentCommands[] = array(
                 'name'    => Craft::t('Settings') . ': ' . Craft::t('Sections') . ' - ' . Craft::t('Edit entry type'),
                 'more'    => true,
